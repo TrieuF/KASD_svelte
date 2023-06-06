ad03ea95b0a47ba9d7f1a514bfb5adb60acad2ae
TrieuF <jonnytrieu@live.de>
2023-06-06 16:32:03 +0200

    added forgotten auth-test api

--- test/api/auth-test.js
+++ test/api/auth-test.js
@@ -12,14 +12,28 @@ suite("Authentication API tests", async () => {
     });
 
     test("authenticate", async () => {
-
+        const returnedUser = await placemarkService.createUser(maggie);
+        const response = await placemarkService.authenticate(maggieCredentials);
+        assert(response.success);
+        assert.isDefined(response.token);
     });
 
     test("verify Token", async () => {
+        const returnedUser = await placemarkService.createUser(maggie);
+        const response = await placemarkService.authenticate(maggieCredentials);
 
+        const userInfo = decodeToken(response.token);
+        assert.equal(userInfo.email, returnedUser.email);
+        assert.equal(userInfo.userId, returnedUser._id);
     });
 
     test("check Unauthorized", async () => {
-
+        placemarkService.clearAuth();
+        try {
+            await placemarkService.deleteAllUsers();
+            assert.fail("Route not protected");
+        } catch (error) {
+            assert.equal(error.response.data.statusCode, 401);
+        }
     });
 });
