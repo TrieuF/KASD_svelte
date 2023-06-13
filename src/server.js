5090167331cc25eb07e8103affddc7f2f75ea9c2
TrieuF <jonnytrieu@live.de>
2023-06-13 22:43:48 +0200

    ts

--- src/server.js
+++ src/server.js
@@ -40,7 +40,7 @@ const swaggerOptions = {
 }
 async function init() {
     const server = Hapi.server({
-        port: process.env.PORT || 3000,
+        port: process.env.PORT || 4000,
     });
     await server.register(Vision);
     await server.register(Cookie);
